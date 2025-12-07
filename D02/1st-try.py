f77a6f620cb4e6801b3551a7c7becc6407437825
mirror <hunter@mir.ror>
2025-12-07 14:20:59 -0400

    archivos iniciales dia 2

---
+++ D02/1st-try.py
@@ -0,0 +1,4 @@
+# necesitas encontrar patrones dentro de los rangos de números
+# los patrones que encuentres deberás sumarlo
+# el resultado de esa suma será tu respuesta
+# ejm: 11+22+99+1010+1188511885+222222+446446+38593859 = 1227775554
